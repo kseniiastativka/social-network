a80ef0785b54597386e8f2615b4102e560408bfd
Kseniia Stativka <kseniia.stativka@gmail.com>
2021-03-20 13:18:17 +0100

    feat: Introduce routes

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-import s from './Profile.module.css'
 import MyPosts from "./MyPosts/MyPosts";
 
 const Profile = () => {
     return (<>
-        <div className={s.content}>
+        <div >
             <img width='800'
                  src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"
                  alt=""/>
