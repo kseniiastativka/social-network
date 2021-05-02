22502f2fdec88201e9a776b156b00ac89863155d
Kseniia Stativka <kseniia.stativka@gmail.com>
2021-05-02 16:18:56 +0200

    test: add stupid test

---
+++ src/redux/profile-reducer.test.ts
@@ -0,0 +1,17 @@
+import { addPostActionCreator, profileReducer } from "./profile-reducer";
+let initialState = {
+  posts: [
+    { id: 1, message: "Miss me?", likesCount: 12 },
+    { id: 2, message: "I have a vacation soon!", likesCount: 34 },
+    { id: 3, message: "I want banana bread", likesCount: 7 },
+  ],
+  profile: undefined,
+  status: "",
+};
+
+it("new post should be added ", () => {
+  let action = addPostActionCreator("it-kamasutra.com");
+  let newState = profileReducer(initialState, action);
+  expect(newState.posts.length).toBe(4);
+  expect(newState.posts[3].message).toBe("it-kamasutra.com");
+});
