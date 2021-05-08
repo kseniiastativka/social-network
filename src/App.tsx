a3eaa7821add166b2e09f3b54d889ae265a7f7f4
Kseniia Stativka <kseniia.stativka@gmail.com>
2021-05-08 14:07:35 +0200

    refactor: add lazy loading for Profile and Dialog components

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,8 @@ import { Route, withRouter } from "react-router-dom";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import NavbarContainer from "./components/Navbar/NavbarContainer";
 import UsersContainer from "./components/Users/UsersContainer";
-import ProfileComponent from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import { Login } from "./components/Login/Login";
 import { connect } from "react-redux";
@@ -16,6 +14,13 @@ import { compose } from "redux";
 import { initialiseApp } from "./redux/app-reducer";
 import { State } from "./redux/redux-store";
 import Spinner from "./components/common/Spinner/Spinner";
+
+const DialogsContainer = React.lazy(
+  () => import("./components/Dialogs/DialogsContainer")
+);
+const ProfileComponent = React.lazy(
+  () => import("./components/Profile/ProfileContainer")
+);
 
 interface AppComponentProps extends RouteComponentProps {
   initialiseApp: () => void;
@@ -36,8 +41,26 @@ class App extends React.Component<AppComponentProps> {
         <HeaderContainer />
         <NavbarContainer />
         <div className="app-wrapper-content">
-          <Route path="/dialogs" render={() => <DialogsContainer />} />
-          <Route path="/profile/:userId?" render={() => <ProfileComponent />} />
+          <Route
+            path="/dialogs"
+            render={() => {
+              return (
+                <React.Suspense fallback={<Spinner />}>
+                  <DialogsContainer />
+                </React.Suspense>
+              );
+            }}
+          />
+          <Route
+            path="/profile/:userId?"
+            render={() => {
+              return (
+                <React.Suspense fallback={<Spinner />}>
+                  <ProfileComponent />
+                </React.Suspense>
+              );
+            }}
+          />
           <Route path="/news" render={() => <News />} />
           <Route path="/music" render={() => <Music />} />
           <Route path="/settings" render={() => <Settings />} />
